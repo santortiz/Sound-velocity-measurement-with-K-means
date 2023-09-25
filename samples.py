d4c199e933c1f273538b3149548f0b11804741f5
santiago <santiago.ortiz@guane.com.co>
2023-09-25 16:58:58 -0500

    Desarrollo algoritmos calirbación e identifiación picos

---
+++ samples.py
@@ -0,0 +1,36 @@
+import serial
+import time
+#Configure the serial port. Replace 'COM3' with the appropriate port name on your system.
+ser = serial.Serial('/dev/ttyACM0', 9600)
+
+
+lines_per_sample = []
+n_sample=0
+while n_sample < 100:
+    init_time = time.time()
+    final_time = 0
+    print("muestra tal: ", n_sample)
+    with open(f"samples/sample_{n_sample}.txt", "w") as sample_file:
+        n_lines= 0
+        while final_time - init_time < 3:
+            try:
+                line = ser.readline().decode()
+                n_lines += 1
+            except Exception as e:
+                print(e)
+                continue
+            sample_file.write(line)
+            sample_file.flush()
+            final_time = time.time()
+    lines_per_sample.append(n_lines)
+    n_sample += 1
+
+with open("samples/lines_per_sample.txt", "w") as lines_file:
+    lines_file.write(str(lines_per_sample))
+    lines_file.flush()
+ser.close()
+
+
+
+
+
--- sensor.py
+++ sensor.py
@@ -1,28 +1,18 @@
 import serial
 import time
-#Configure the serial port. Replace 'COM3' with the appropriate port name on your system.
+
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
-# Create a text file to store the sensor data
-with open('sensor_data.txt', 'w') as file:
-    final_time=0
-    init_time = time.time()
-    while final_time - init_time < 5:
-        # Read a line of data from the Arduino via serial
-        line = ser.readline().decode('utf-8')
-        
-        # Write the data to the text file
-        file.write(line)
-        file.flush()  # Ensure data is written immediately
-        
-        # Print the data to the console (optional)
-        print(line, end='')
+init_time = time.time()
+final_time = 0
+with open("sensor_data.txt", "w") as sensor_file:
+    while final_time - init_time < 30:
+        try:
+            line = ser.readline().decode()
+            print(line)
+        except Exception as e:
+            print(e)
+        sensor_file.write(line)
+        sensor_file.flush()
         final_time = time.time()
-
-# Close the serial port when done
 ser.close()
-
-
-
-
-
